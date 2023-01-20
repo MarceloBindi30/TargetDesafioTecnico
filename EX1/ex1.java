362b0032e39412bb28a784a5861c1a0850c857ae
Marcelo Bindi Leal <mbindileal10@gmail.com>
2023-01-20 13:17:08 -0300

    Ex1 e Ex2

---
+++ EX1/ex1.java
@@ -0,0 +1,17 @@
+package EX1;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int INDICE = 13;
+        int K = 0;
+        int SOMA = 0;
+    
+        for(K = 0; K < INDICE; K++){
+        SOMA = SOMA + K;
+        }
+    
+        System.out.println(SOMA);
+    
+    }
+    
+}
